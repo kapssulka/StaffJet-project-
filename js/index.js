546979b9541a68054e86ee5db2689031b933c0a0
Danil Tsybulin <tsybulin2003@mail.ru>
2025-03-21 16:36:28 +0600

    Created blocks: “Hiro Section”, “About Us”.

---
+++ js/index.js
@@ -0,0 +1,31 @@
+const timezoneSpan = document.getElementById("js-timezone");
+
+if (timezoneSpan) {
+  function updateTime() {
+    timezoneSpan.innerText = getTime();
+  }
+
+  updateTime();
+
+  // Вычисляем, сколько осталось до следующей минуты
+  const now = new Date();
+  const msToNextMinute = (60 - now.getSeconds()) * 1000;
+
+  setTimeout(() => {
+    updateTime();
+    setInterval(updateTime, 60000);
+  }, msToNextMinute);
+}
+function getTime() {
+  const options = {
+    timeZone: "Europe/Kaliningrad",
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: false,
+  };
+  const time = new Date()
+    .toLocaleString("ru-RU", options)
+    .replace(/^(\d):/, "0$1");
+
+  return time;
+}
